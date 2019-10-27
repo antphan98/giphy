c1c908047def7ba9e0475e5a28d2ece7ddb32159
An Phan <antphan98@gmail.com>
2019-10-27 12:53:09 -0500

    added buttons for array of topics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
-alert("cu");
+
+    const topics = ["sushi", "pizza", "burgers", "sandwich", "pasta"];
+
+    function loadButtons() {
+
+        $("#buttons-view").empty();
+
+        for (let i = 0; i < topics.length; i++) {
+          const newButton = $("<button>");
+          newButton.addClass("food");
+          newButton.attr("data-name", topics[i]);
+          newButton.text(topics[i]);
+          $("#buttons-view").append(newButton);
+        }
+      }
+
+loadButtons();
+
